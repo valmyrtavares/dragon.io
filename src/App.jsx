d86cadacaa57452b69a0102061b8b9627215f1a9
valmyrtavaes <valmyrtavares71@gmail.com>
2025-02-28 14:36:56 -0300

    build

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <>
       <div>
-        <Router basename="/">
+        <Router basename="/dragon.io">
           <Header />
           <Routes>
             <Route path="/" element={<ShowCase />} />
