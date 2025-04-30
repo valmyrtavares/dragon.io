ba2b385464eca17a5d94f786e675b9a0db4c43a6
valmyrtavaes <valmyrtavares71@gmail.com>
2025-04-30 12:40:43 -0300

    build-github

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const rootElement = document.getElementById('root');
+console.log('ROOT ELEMENT:', rootElement);
+
+if (!rootElement) {
+  alert('Elemento #root NÃO encontrado!');
+} else {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
